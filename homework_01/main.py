4a00e35bb9c3e29e4df83136078a7df1e738ec64
Zueva Darya <dzueva@aq.ru>
2024-05-23 11:23:32 +0300

    dz1

--- homework_01/main.py
+++ homework_01/main.py
@@ -2,15 +2,20 @@
 Домашнее задание №1
 Функции и структуры данных
 """
+from math import sqrt
 
 
-def power_numbers():
+def power_numbers(*numbers):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
+    result = []
+    for num in numbers:
+        result.append(int(num)**2)
+    return result
 
 
 # filter types
@@ -19,7 +24,16 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
+def is_prime(number):
+    if number <= 1:
+        return False
+    for i in range(2, int(sqrt(number)) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
+def filter_numbers(*numbers, filtr):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -30,3 +44,11 @@ def filter_numbers():
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+    result = []
+    if filtr == PRIME:
+        result = list(filter(is_prime, numbers))
+    elif filtr == EVEN:
+        result = [i for i in numbers if i % 2 == 0]
+    elif filtr == ODD:
+        result = [i for i in numbers if i % 2 != 0]
+    return result
